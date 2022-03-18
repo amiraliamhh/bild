b388b286444106756b0c98b55e850678a41dca17
amirali <amiraliamhh@gmail.com>
2022-03-19 03:17:38 +0330

    fix(trial): responsive

--- src/components/shared/Trial/index.tsx
+++ src/components/shared/Trial/index.tsx
@@ -2,9 +2,9 @@ import { Button } from '@shared'
 
 export const Trial = () => (
   <section className='w-full bg-zinc-800 pt-12 pb-[3.5rem]'>
-    <div className='container mx-auto flex justify-between items-center'>
-      <h3 className='text-2xl text-white font-semibold uppercase'>Are You Ready To Be Blown Away?</h3>
-      <Button className='px-4'>
+    <div className='container mx-auto flex flex-col sm:flex-row justify-between items-center'>
+      <h3 className='text-2xl text-white font-semibold text-center uppercase'>Are You Ready To Be Blown Away?</h3>
+      <Button className='px-4 mt-4 sm:mt-0'>
         CLICK HERE TO FIND OUT
       </Button>
     </div>
